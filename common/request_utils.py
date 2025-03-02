9bcf37e01fb8bb057169f44dbe4d9ddde0010b05
Henry Page <h.page@student.tudelft.nl>
2025-03-03 00:37:56 +0100

    get rid of request statements in utils

--- common/request_utils.py
+++ common/request_utils.py
@@ -10,18 +10,14 @@ def process_encoded_response_body(response):
 
     status_code = int(response_body['status'])
 
-
     # Check if the status code is a 4xx error
     if 400 <= status_code < 500:
-        logging.error("YO3")
         # Handle the client error by aborting with the appropriate status and message
         abort(status_code, response_body['error'])
-
 
     if response_body['is_json']:
         return jsonify(response_body.get("content", "{}"))
     else:
-        logging.error("YO6")
         return Response(response_body.get("content", ""), status=status_code)
 
 def create_response_message(content, is_json, status: int = 200):
